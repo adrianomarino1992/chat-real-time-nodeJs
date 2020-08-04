583638b6f1779ad218a228379ec6cb48c223c2b6
Adriano Marino Balera <adriano.marino1992@gmail.com>
2020-08-04 13:50:56 -0300

    corrigindo porta

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ let App = require('./Models/App');
 
 let Routers = require('./Rotas/Rotas');
 
-App.Start(1235); // para testes na rede local'
+//App.Start(1235); // para testes na rede local'
 
-//App.Start(process.env.PORT); // para deploy use apenas 'process.env.PORT'
+App.Start(process.env.PORT); // para deploy use apenas 'process.env.PORT'
 
 Routers.Start(App.App(),__dirname,Routers);
 
