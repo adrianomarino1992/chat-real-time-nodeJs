300f6f228d9742483e5bf2a743be71b11ec53792
Adriano Marino Balera <adriano.marino1992@gmail.com>
2020-08-17 15:51:53 -0300

    Bugs

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ let App = require('./Models/App');
 
 let Routers = require('./Rotas/Rotas');
 
+App.Start(1235); // para testes na rede local'
+
 App.Start(process.env.PORT); // para deploy use apenas 'process.env.PORT'
 
 Routers.Start(App.App(),__dirname,Routers);
